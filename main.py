b17cb7669bc307fe48a0fc3afd2da77723ec8a92
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-21 13:26:50 +1100

    Implemented Deck class with shuffle and deal functions for Blackjack.

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 import random
 
-cards = []
+class Deck:
+    def __init__(self):
+        self.cards = []
 
-suits = ["hearts", "spades", "clubs", "diamonds"]  
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        suits = ["hearts", "spades", "clubs", "diamonds"]  
+        ranks = [
+                    {"rank": "A", "value": 11},
+                    {"rank": "2", "value": 2},
+                    {"rank": "3", "value": 3},
+                    {"rank": "4", "value": 4},
+                    {"rank": "5", "value": 5},
+                    {"rank": "6", "value": 6},
+                    {"rank": "7", "value": 7},
+                    {"rank": "8", "value": 8},
+                    {"rank": "9", "value": 9},
+                    {"rank": "10", "value": 10},
+                    {"rank": "J", "value": 10},
+                    {"rank": "Q", "value": 10},
+                    {"rank": "K", "value": 10},
+                ]
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
-def shuffle():
-    random.shuffle(cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
-
-if rank == "A":
-    value = 11 
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
-
-rank_direct = {"rank": rank, "value": value}
-
-print(rank_direct["rank"], rank_direct["value"])
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            card = self.cards.pop()
+            cards_dealt.append(card)
+        return cards_dealt
