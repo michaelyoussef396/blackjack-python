1e5e069710a57d60da73c4a7610852dfe8d42ba1
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-21 13:16:07 +1100

    Implemented card dealing, value assignment, and fixed variable naming for Blackjack game.

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+cards = []
+
+suits = ["hearts", "spades", "clubs", "diamonds"]  
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+
+def shuffle():
+    random.shuffle(cards)
+
+def deal(number):
+    cards_dealt = []
+    for x in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+shuffle()
+cards_dealt = deal(2)
+card = cards_dealt[0]
+rank = card[1]
+
+if rank == "A":
+    value = 11 
+elif rank == "J" or rank == "Q" or rank == "K":
+    value = 10
+else:
+    value = rank
+
+rank_direct = {"rank": rank, "value": value}
+
+print(rank_direct["rank"], rank_direct["value"])
